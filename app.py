5683a38fd47d25488a0ef5d7cf5aafd647531f3b
Rajan Pande <panderajan1996@gmail.com>
2025-11-20 14:51:54 -0800

    added cleanup endpoints

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from notifier_module.notifier_api import app as notifier_api
 from user_manager.users_api import app as user_api
+from cleanup_module.cleaning_api import app as cleaner_api
 
 app = FastAPI()
 origins = ["*"]
@@ -17,6 +18,7 @@ app.add_middleware(
 
 app.include_router(notifier_api)
 app.include_router(user_api)
+app.include_router(cleaner_api)
 
 
 if __name__ == "__main__":
